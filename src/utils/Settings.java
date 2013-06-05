6e1516ce6a01366dcc36709425e48db5a41ed5be
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-06-05 17:09:09 +0200

    Changed grammatic error

--- src/utils/Settings.java
+++ src/utils/Settings.java
@@ -3,7 +3,7 @@ package utils;
 public class Settings {
 
 	/**
-	 * Constant value used when we couldnt resolve a binding
+	 * Constant value used when we could not resolve a binding
 	 */
 	public static final boolean DEFAULT_BINDING_VALUE = false;
 
