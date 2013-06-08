debff622966752705a44cbc626bb94239a1e1ccc
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-06-08 16:14:49 +0200

    Created a new application used by Alessandra to run on a VM

---
+++ src/AlessandraVMApplication.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FilenameUtils;
+import org.eclipse.jdt.core.JavaCore;
+
+import utils.Settings;
+
+
+public class AlessandraVMApplication {
+
+	private static final String PREFIX = "pre-fix";
+	private static final String POSTFIX = "post-fix";
+	
+	/**
+	 * Runs an check for one specific iBugs ID.
+	 * @param args Expected parameters:<p>
+	 * 1) path to the folder containing all ID folders<p>
+	 * 2) the iBugs ID to check<p>
+	 * 3) path to the folder containing the sources <p>
+	 * <p>
+	 * I assume a folder hierarchy in the following way: <p>
+	 * ./ID/prefix/prefix.jar <p>
+	 * ./ID/prefix/all different .java files <p>
+	 * ./ID/postfix/postfix.jar <p>
+	 * ./ID/postfix/all different .java files <p>
+	 */
+	public static void main(String[] args) {
+		// parse the parameters
+		String pathToIDFolders 	= args[0];
+		if(pathToIDFolders == null){
+			System.out.println("Please set up the first parameter.");
+			System.exit(0);
+		}
+		int iBugs_ID = 0;
+		try {
+			iBugs_ID = Integer.parseInt(args[1]);
+		} catch (NumberFormatException e1) {
+			System.out.println("Please set up a correct integer as second parameter.");
+			System.exit(0);			
+		}
+		String pathToSources 	= args[2];
+		if(pathToSources == null){
+			System.out.println("Please set up the third parameter.");
+			System.exit(0);
+		}
+		
+		
+		// extract the different paths
+		String initialPath = pathToIDFolders + "\\" + iBugs_ID;
+		String prefixFolder  = initialPath + "\\" + PREFIX;
+		String postfixFolder = initialPath + "\\" + POSTFIX;
+		File[] prefixFoldercontent  = new File(prefixFolder).listFiles();
+		File[] postfixFoldercontent = new File(postfixFolder).listFiles();
+		String prefixSourceFolder 	= pathToSources + "\\" + PREFIX;
+		String postfixSourceFolder 	= pathToSources + "\\" + POSTFIX;
+		
+		// initialize jMutOps
+		JMutOps jmutops = new JMutOps();
+		jmutops.initProgram("iBugs");
+		jmutops.setIncludeRunningVMBootclasspath(true);
+	    Hashtable<String, String> options = JavaCore.getDefaultOptions();
+	    options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_6);
+		jmutops.setOptions(options, JMutOps.TargetVersion.PREFIX);
+		jmutops.setOptions(options, JMutOps.TargetVersion.POSTFIX);  
+		
+		// look for source folders in pathToSources
+		checkForSrc(jmutops, new File[]{new File(prefixSourceFolder)}, JMutOps.TargetVersion.PREFIX);
+		checkForSrc(jmutops, new File[]{new File(postfixSourceFolder)}, JMutOps.TargetVersion.POSTFIX);
+		
+		// initialize the logger
+		Logger logger = Logger.getLogger(TestApplication.class.getName());
+		// set the handler for the root logger
+		try {
+			Logger tempAnonymousLogger = Logger.getAnonymousLogger();
+			Handler handler = new FileHandler("log_" + iBugs_ID + ".txt");
+			tempAnonymousLogger.getParent().addHandler(handler);
+			tempAnonymousLogger.getParent().setLevel(Settings.loggerlevel);
+			logger.info("Set FileHandler for logger.");
+		} catch (SecurityException e) {
+			logger.info("Could not set FileHandler for logger.");
+		} catch (IOException e) {
+			logger.info("Could not set FileHandler for logger.");
+		}
+		
+		logger.info("Starting to check iBugs ID " + iBugs_ID + "\n");
+		
+		// check each file in the prefix folder
+		for(File prefixFile: prefixFoldercontent){
+			File postfixFile = new File(postfixFolder + "\\" + prefixFile.getName());
+			
+			// check for file existance
+			if((!prefixFile.exists()) || (!postfixFile.exists())){
+				continue;
+			}
+			
+			// check both files to be .java
+			if( !(FilenameUtils.isExtension(prefixFile.getName(),"java")) || !(FilenameUtils.isExtension(postfixFile.getName(),"java"))){
+				continue;
+			}
+			
+			logger.fine("Starting to check file " + prefixFile.getName() + ".");
+			
+			jmutops.checkFiles(prefixFile, postfixFile);
+							
+			logger.fine("Ending to check file " + postfixFile.getName() + ".");
+		}
+		
+		logger.info("Ending to check iBugs ID " + iBugs_ID + "\n");
+		
+	}
+	
+	public static void checkForSrc(JMutOps jmutops, File[] folderContent, JMutOps.TargetVersion version){
+		for(File file: folderContent){
+			if(file.isDirectory()){
+				if(file.getName().equals("src")){
+					jmutops.addSourcepathEntry(file.getAbsolutePath(), null, version);
+				}else{
+					checkForSrc(jmutops, file.listFiles(), version);
+				}
+			}
+		}
+	}
+
+}
