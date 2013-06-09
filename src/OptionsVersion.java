86a1f401241f35ff8d278e533d2eb409fb51d363
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-06-09 20:13:09 +0200

    Removed some Files

--- src/OptionsVersion.java
+++
@@ -1,8 +0,0 @@
-/**
- * Enumeration used in case of adding classpath / sourcepath to distinguish between prefixed and postfixed code. 
- * @author Lukas Subel
- *
- */
-public enum OptionsVersion {
-	PREFIX, POSTFIX;
-}
