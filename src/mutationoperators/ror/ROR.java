94b36e3c87db3d207f5aa53d5cccbe69e5f11c30
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-15 00:22:33 +0200

    Implemented ROR; need some test cases for it

---
+++ src/mutationoperators/ror/ROR.java
@@ -0,0 +1,19 @@
+package mutationoperators.ror;
+
+import results.JMutOpsEventListenerMulticaster;
+import enums.MutationOperatorCategory;
+import enums.MutationOperatorLevel;
+import mutationoperators.MutationOperator;
+import mutationoperators.lco.LCO_Matcher;
+import mutationoperators.lco.LCO_Visitor;
+
+public class ROR extends MutationOperator {
+
+	public ROR(JMutOpsEventListenerMulticaster eventListener) {
+		super("Relational Operator Replacement", "ROR", "Replace relational operators with other relational operators, and replace the entire predicate with true and false", 
+				MutationOperatorLevel.METHOD_LEVEL, eventListener, MutationOperatorCategory.METHOD_LEVEL);
+		this.matcher = new ROR_Matcher(this);
+		this.visitor = new ROR_Visitor(this.matcher);
+	}
+
+}
---
+++ src/mutationoperators/ror/ROR_Visitor.java
@@ -0,0 +1,40 @@
+package mutationoperators.ror;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.InfixExpression;
+
+import mutationoperators.BaseASTMatcher;
+import mutationoperators.BaseASTVisitor;
+
+public class ROR_Visitor extends BaseASTVisitor {
+
+	public ROR_Visitor(BaseASTMatcher matcher) {
+		super(matcher);
+	}
+
+	@Override
+	public boolean visit(InfixExpression node) {
+		// locally store the AST
+		ASTNode localStoredTree = getSecondTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof InfixExpression){
+			InfixExpression ie = (InfixExpression) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, ie);
+			
+			// visit the left operator
+			visitSubtree(node.getLeftOperand(), ie.getLeftOperand());
+			
+			// visit the right operator
+			visitSubtree(node.getRightOperand(), ie.getRightOperand());
+			
+			// visit the extended expression
+			visitSubtrees(node.extendedOperands(), ie.extendedOperands());			
+		}
+		
+		super.visit(node);
+		return true;
+	}
+}
