be92489ff11aaa5a9448b45844db0548376b08cc
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-15 14:43:33 +0200

    Created tests for COD

---
+++ testsrc/mutationoperators/COD_Test.java
@@ -0,0 +1,77 @@
+package mutationoperators;
+
+import static org.junit.Assert.*;
+
+import mutationoperators.cod.COD;
+
+import org.junit.Test;
+
+public class COD_Test extends BasicMutationOperatorTest {
+
+	@Override
+	protected String getOperatorName() {
+		return new COD().getShortname();
+	}
+
+	@Override
+	protected String getFields() {
+		return  "int a1 = 0; " +
+				"int a2 = 9; " +
+				"boolean b1 = true; " +
+				"boolean b2 = false; ";
+	}
+
+	@Test
+	public void testCOR_constantComparision1(){
+		int diff = compareMatches("boolean result = !(true && false); System.out.println();", "boolean result = (true && false); System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_constantComparision2(){
+		int diff = compareMatches("boolean result = !(true & false); System.out.println();", "boolean result = (true & false); System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_constantComparision3(){
+		int diff = compareMatches("boolean result = !(false | false); System.out.println();", "boolean result = (false | false); System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_constantComparision4(){
+		int diff = compareMatches("boolean result = !(true || true); System.out.println();", "boolean result = (true || true); System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_constantComparision5(){
+		int diff = compareMatches("boolean result = !(true ^ true); System.out.println();", "boolean result = (true ^ true); System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_negateConstant1(){
+		int diff = compareMatches("boolean result = !true; System.out.println();", "boolean result = true; System.out.println();");
+		assertEquals(1, diff);
+	}
+
+	@Test
+	public void testCOR_negateConstant2(){
+		int diff = compareMatches("boolean result = !false; System.out.println();", "boolean result = false; System.out.println();");
+		assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testCOR_multiple1(){
+		int diff = compareMatches("boolean result = !b1 || !b2; System.out.println();", "boolean result = b1 || b2; System.out.println();");
+		assertEquals(2, diff);
+	}
+	
+	@Test
+	public void testCOR_multiple2(){
+		int diff = compareMatches("boolean result = !(!(b1)); System.out.println();", "boolean result = ((b1)); System.out.println();");
+		assertEquals(2, diff);
+	}
+}
