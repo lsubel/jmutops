ac92d44fd584972c6edcdcccf94984156cb84150
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-15 22:30:43 +0200

    Fixed some test cases to support the detection of ChangeDistiller Added some more tests for AOD

--- testsrc/mutationoperators/AOD_Test.java
+++ testsrc/mutationoperators/AOD_Test.java
@@ -45,49 +45,37 @@ public class AOD_Test extends BasicMutationOperatorTest {
 	
 	@Test
 	public void testAOD_shortcutDeletion1() {
-		int diff = compareMatches("this.b++;", "this.b;");
+		int diff = compareMatches("System.out.println(); this.b = ++this.a;", "System.out.println(); this.b = this.a;");
 		assertEquals(1, diff);
 	}
 	
 	@Test
 	public void testAOD_shortcutDeletion2() {
-		int diff = compareMatches("this.b--;", "this.b;");
+		int diff = compareMatches("this.b = --this.a; System.out.println();", "this.b = this.a; System.out.println();");
 		assertEquals(1, diff);
 	}
 	
 	@Test
 	public void testAOD_shortcutDeletion3() {
-		int diff = compareMatches("++this.a;", "this.a;");
+		int diff = compareMatches("this.b = this.a--; System.out.println();", "this.b = this.a; System.out.println();");
 		assertEquals(1, diff);
 	}
 	
 	@Test
 	public void testAOD_shortcutDeletion4() {
-		int diff = compareMatches("--this.a;", "this.a;");
+		int diff = compareMatches("this.b = this.a++; System.out.println();", "this.b = this.a; System.out.println();");
 		assertEquals(1, diff);
 	}
 	
 	@Test
-	public void testAOD_shortcutDeletion5() {
-		int diff = compareMatches("b = ++a;", "b = a;");
+	public void testAOD_shortcutDeletionForLoop1() {
+		int diff = compareMatches("for(int i = 0; i<a--; i++){System.out.println();}", "for(int i = 0; i<a; i++){System.out.println();}");
 		assertEquals(1, diff);
 	}
 	
 	@Test
-	public void testAOD_shortcutDeletion6() {
-		int diff = compareMatches("b = --a;", "b = a;");
-		assertEquals(1, diff);
-	}
-	
-	@Test
-	public void testAOD_shortcutDeletion7() {
-		int diff = compareMatches("b = a--;", "b = a;");
-		assertEquals(1, diff);
-	}
-	
-	@Test
-	public void testAOD_shortcutDeletion8() {
-		int diff = compareMatches("b = a++;", "b = a;");
+	public void testAOD_shortcutDeletionWhileLoop1() {
+		int diff = compareMatches("while(a++ < b){System.out.println();b-=1;}", "while(a < b){System.out.println();b-=1;}");
 		assertEquals(1, diff);
 	}
 }
