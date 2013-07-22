df9b034664dadb30560566a5139439afdea4065d
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-22 11:12:39 +0200

    Implemented AFRO_Visitor and AFRO_Matcher

--- src/mutationoperators/afro/AFRO_Visitor.java
+++ src/mutationoperators/afro/AFRO_Visitor.java
@@ -3,10 +3,17 @@ package mutationoperators.afro;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.BaseASTVisitor;
 
+import org.eclipse.jdt.core.dom.FieldAccess;
+
 public class AFRO_Visitor extends BaseASTVisitor {
 
 	public AFRO_Visitor(BaseASTMatcher matcher) {
 		super(matcher);
 	}
 
+	@Override
+	public boolean visit(FieldAccess node) {
+		matcher.match(node, parallelTree);
+		return false;
+	}
 }
