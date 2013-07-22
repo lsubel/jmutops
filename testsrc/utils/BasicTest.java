361e71a2d1b42b0e6c3dd6c93a19291a47f7b780
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-22 16:01:44 +0200

    Created new basic test class

---
+++ testsrc/utils/BasicTest.java
@@ -0,0 +1,153 @@
+package utils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import jmutops.JMutOps;
+import mutationoperators.MutationOperator;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.junit.After;
+import org.junit.Before;
+
+import results.ApplicationCounter;
+import enums.OptionsVersion;
+
+public abstract class BasicTest {
+
+	/////////////////////////////////////////
+	/// Constants
+	/////////////////////////////////////////
+	
+	protected static final File PATH_FOR_PREFIX_FILES = TestUtilities.getFolder(TestUtilities.getTempDir() + File.separator + "prefix");
+
+	protected static final File PATH_FOR_POSTFIX_FILES = TestUtilities.getFolder(TestUtilities.getTempDir() + File.separator + "postfix");
+	
+	/////////////////////////////////////////
+	/// Fields
+	/////////////////////////////////////////
+	
+	JMutOps jmutops;
+	
+	ApplicationCounter counter;
+	
+	ArrayList<File> tempfilelist;
+	
+	
+	/////////////////////////////////////////
+	/// Methods
+	/////////////////////////////////////////	
+
+	@Before
+	public void setUp() throws Exception {
+		// initialize arraylist which contains all temporary created files
+		this.tempfilelist = new ArrayList<File>();
+		
+		// initialize ApplicationCounter which counts the number of applied mutation operators
+		this.counter = new ApplicationCounter();
+		
+		// initialize jMutOps
+		this.jmutops = new JMutOps();
+		this.jmutops.initProgram("Internal testSuite", "Used during the JUnitTests", "", "");
+		this.jmutops.setIncludeRunningVMBootclasspath(true);
+	    Hashtable<String, String> options = JavaCore.getDefaultOptions();
+	    options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_6);
+	    this.jmutops.setOptions(options, OptionsVersion.PREFIX);
+	    this.jmutops.setOptions(options, OptionsVersion.POSTFIX); 
+	    
+	    // add the ApplicationsCounter to jMutOps so we can count the applications
+		this.jmutops.addResultListener(counter);
+		
+		// initialize test specific context 
+		initializeContext();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// delete all temporary files
+		for(File tempfile: this.tempfilelist){
+			tempfile.delete();
+		}
+		// remove objects
+		this.jmutops = null;
+		this.counter = null;
+	}
+
+	/**
+	 * Write the {@code javaCode} into a temporary file named {@code fileName} into the folder {@code pathToFolder}.
+	 * @param fileName The filename to the temporary file.
+	 * @param javaCode The java code which should be written into the temporary file. 
+	 * @param pathToFolder The path where the temporary file should be saved.
+	 * @return The temporary file.
+	 */
+	protected File createSourceFile(String fileName, String javaCode, File pathToFolder) {
+		// create new tempfile
+		File tempfile = new File(pathToFolder, fileName + ".java");
+		// add the temp file to the tempfile array so we can delete it at the end
+		this.tempfilelist.add(tempfile);
+		
+		// write sourcecode into tempfile
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(tempfile));
+			out.write(javaCode);
+			out.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		
+		// return the tempfile
+		return tempfile;
+	}
+	
+	/**
+	 * Add a source file to jMutOps with {@code fileName} as name of file 
+	 * and {@code fileContent} as the content of the file.
+	 * @param fileName
+	 * @param fileContent
+	 */
+	protected void addContextSourceFile(String fileName, String fileContent){
+		// create files for both versions
+		File prefix = createSourceFile(fileName, fileContent, this.PATH_FOR_PREFIX_FILES);
+		File postfix = createSourceFile(fileName, fileContent, this.PATH_FOR_POSTFIX_FILES);
+		
+		// add both files to jMutOps
+		this.jmutops.addSourcepathEntry(prefix.getAbsolutePath(), "", OptionsVersion.PREFIX);
+		this.jmutops.addSourcepathEntry(postfix.getAbsolutePath(), "", OptionsVersion.POSTFIX);
+	}
+
+	
+	/**
+	 * Run a prefix and postfix version on jMutOps and returns the number of applications 
+	 * of the tested mutation operator.
+	 * @param preFix The prefix version of code.
+	 * @param postFix The postfix version of code.
+	 * @return The number of applications >= 0.
+	 */
+	public int compareMatches(File preFix, File postFix) {
+		// stores the current count for this mutation operator
+		int oldValue = this.counter.getCount(this.getTestedMutationOperator().getShortname()).intValue();
+		// run jMutOps on both files
+		this.jmutops.checkFiles(preFix, postFix);
+		// returns the difference between both versions
+		return this.counter.getCount(this.getTestedMutationOperator().getShortname()).intValue() - oldValue;
+	}
+	
+	/**
+	 * Get the tested {@link MutationOperator}.
+	 * @return The {@link MutationOperator} under test.
+	 */
+	protected abstract MutationOperator getTestedMutationOperator();
+	
+	/**
+	 * Initialized context for the class under test,
+	 * e.g. fields in the same file or classes in other files. <p>
+	 * Will be called in the @Before method of {@link BasicTest}.
+	 */
+	protected abstract void initializeContext();
+
+}
