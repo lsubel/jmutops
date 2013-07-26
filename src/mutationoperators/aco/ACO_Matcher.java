8da8fcf21d64a047f453a1a8ab6eb1be85888706
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-07-26 12:37:38 +0200

    Started to implement ACO

---
+++ src/mutationoperators/aco/ACO_Matcher.java
@@ -0,0 +1,12 @@
+package mutationoperators.aco;
+
+import mutationoperators.BaseASTMatcher;
+import mutationoperators.MutationOperator;
+
+public class ACO_Matcher extends BaseASTMatcher {
+
+	public ACO_Matcher(MutationOperator mutop) {
+		super(mutop);
+	}
+
+}
---
+++ src/mutationoperators/aco/ACO_Visitor.java
@@ -0,0 +1,12 @@
+package mutationoperators.aco;
+
+import mutationoperators.BaseASTMatcher;
+import mutationoperators.BaseASTVisitor;
+
+public class ACO_Visitor extends BaseASTVisitor {
+
+	public ACO_Visitor(BaseASTMatcher matcher) {
+		super(matcher);
+	}
+
+}
