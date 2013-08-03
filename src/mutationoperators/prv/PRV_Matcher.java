3f248e6c5bf8d9aaad4fd8e052796baf77c8ba7d
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-03 22:15:34 +0200

    Implemented PRV_Matcher and PRV_Visitor

--- src/mutationoperators/prv/PRV_Matcher.java
+++ src/mutationoperators/prv/PRV_Matcher.java
@@ -3,10 +3,122 @@ package mutationoperators.prv;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.MutationOperator;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.SimpleName;
+
 public class PRV_Matcher extends BaseASTMatcher {
 
 	public PRV_Matcher(MutationOperator mutop) {
 		super(mutop);
 	}
 
+	@Override
+	public boolean match(FieldAccess node, Object other) {
+		boolean firstObjectCompatibleToVar;
+		boolean secondObjectCompatibleToVar;
+		
+		// define variables
+		boolean differentVariables;
+		boolean differentClasses;
+		
+		// check for an access to an object on the parallel one
+		// then check if the references classes are different
+		if(other instanceof FieldAccess){
+			FieldAccess node2 = (FieldAccess) other;
+			differentVariables = !(node.getName().subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof QualifiedName){
+			QualifiedName node2 = (QualifiedName) other;
+			differentVariables = !(node.getName().subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof SimpleName){
+			SimpleName node2 = (SimpleName) other;
+			differentVariables = !(node.subtreeMatch(defaultMatcher, node2));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else{
+			return false;
+		}
+		
+		if(differentVariables && differentClasses){
+			mutop.found(node, (ASTNode) other);
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean match(QualifiedName node, Object other) {
+		// define variables
+		boolean differentVariables;
+		boolean differentClasses;
+		
+		// check for an access to an object on the parallel one
+		// then check if the references classes are different
+		if(other instanceof FieldAccess){
+			FieldAccess node2 = (FieldAccess) other;
+			differentVariables = !(node.getName().subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof QualifiedName){
+			QualifiedName node2 = (QualifiedName) other;
+			differentVariables = !(node.getName().subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof SimpleName){
+			SimpleName node2 = (SimpleName) other;
+			differentVariables = !(node.subtreeMatch(defaultMatcher, node2));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else{
+			return false;
+		}
+		
+		if(differentVariables && differentClasses){
+			mutop.found(node, (ASTNode) other);
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean match(SimpleName node, Object other) {
+		// define variables
+		boolean differentVariables;
+		boolean differentClasses;
+		
+		// check for an access to an object on the parallel one
+		// then check if the references classes are different
+		if(other instanceof FieldAccess){
+			FieldAccess node2 = (FieldAccess) other;
+			differentVariables = !(node.subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof QualifiedName){
+			QualifiedName node2 = (QualifiedName) other;
+			differentVariables = !(node.subtreeMatch(defaultMatcher, node2.getName()));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else if(other instanceof SimpleName){
+			SimpleName node2 = (SimpleName) other;
+			differentVariables = !(node.subtreeMatch(defaultMatcher, node2));
+			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+		}
+		else{
+			return false;
+		}
+		
+		if(differentVariables && differentClasses){
+			mutop.found(node, (ASTNode) other);
+			return true;
+		}
+		
+		return false;
+	}
 }
--- src/mutationoperators/prv/PRV_Visitor.java
+++ src/mutationoperators/prv/PRV_Visitor.java
@@ -3,10 +3,92 @@ package mutationoperators.prv;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.BaseASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Assignment;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.SimpleName;
+
 public class PRV_Visitor extends BaseASTVisitor {
 
 	public PRV_Visitor(BaseASTMatcher matcher) {
 		super(matcher);
 	}
 
+	@Override
+	public boolean visit(Assignment node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof Assignment){
+			Assignment assign = (Assignment) localStoredTree;
+			
+			// check for an valid assignment operator (=> normal assignment "=")
+			boolean validAssignmentOperator = (node.getOperator() == Assignment.Operator.ASSIGN) && (assign.getOperator() == Assignment.Operator.ASSIGN);
+			
+			// check if the left sides of both versions are the same
+			boolean sameLeftSide = (node.getLeftHandSide().subtreeMatch(defaultMatcher, assign.getLeftHandSide()));
+			
+			if(validAssignmentOperator && sameLeftSide){
+				// check the right operators
+				visitSubtree(node.getRightHandSide(), assign.getRightHandSide());
+			}		
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(FieldAccess node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for an access to an object on the parallel one
+		if(localStoredTree instanceof FieldAccess){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof QualifiedName){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof SimpleName){
+			matcher.match(node, localStoredTree);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(QualifiedName node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for an access to an object on the parallel one
+		if(localStoredTree instanceof FieldAccess){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof QualifiedName){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof SimpleName){
+			matcher.match(node, localStoredTree);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(SimpleName node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for an access to an object on the parallel one
+		if(localStoredTree instanceof FieldAccess){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof QualifiedName){
+			matcher.match(node, localStoredTree);
+		}
+		else if(localStoredTree instanceof SimpleName){
+			matcher.match(node, localStoredTree);
+		}
+		return false;
+	}
 }
