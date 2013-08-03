c17783dbbc1ff62c970484808083ad2fabc3061d
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-03 23:51:33 +0200

    Implemented PCC_Matcher and PCC_Visitor

--- src/mutationoperators/pcc/PCC_Matcher.java
+++ src/mutationoperators/pcc/PCC_Matcher.java
@@ -3,10 +3,44 @@ package mutationoperators.pcc;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.MutationOperator;
 
+import org.eclipse.jdt.core.dom.CastExpression;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+
 public class PCC_Matcher extends BaseASTMatcher {
 
 	public PCC_Matcher(MutationOperator mutop) {
 		super(mutop);
 	}
+
+	@Override
+	public boolean match(CastExpression node, Object other) {
+		// if the compared AST is no CastExpression,
+		// we cannot compare them
+		if(!(other instanceof CastExpression)){
+			return false;
+		}
+		CastExpression node2 = (CastExpression) other;
+		ITypeBinding firstTypeBinding = node.resolveTypeBinding();
+		ITypeBinding secondTypeBinding = node2.resolveTypeBinding();
+		
+		// check for the same Expression
+		boolean sameExpression = (node.getExpression().subtreeMatch(defaultMatcher, node2.getExpression()));
+		
+		// check for different casted types
+		boolean differentCastTypes = !(firstTypeBinding.isEqualTo(secondTypeBinding));
+		
+		// since the casted types are related, one type has to be the XXX of the other one
+		// TODO: implement a method so this works over both versions
+		boolean validTyping = ((firstTypeBinding.isSubTypeCompatible(secondTypeBinding)) || (secondTypeBinding.isSubTypeCompatible(firstTypeBinding)));
+
+		if(sameExpression && differentCastTypes && validTyping){
+			mutop.found(node, node2);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
 
 }
--- src/mutationoperators/pcc/PCC_Visitor.java
+++ src/mutationoperators/pcc/PCC_Visitor.java
@@ -3,10 +3,28 @@ package mutationoperators.pcc;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.BaseASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CastExpression;
+
 public class PCC_Visitor extends BaseASTVisitor {
 
 	public PCC_Visitor(BaseASTMatcher matcher) {
 		super(matcher);
 	}
+
+	@Override
+	public boolean visit(CastExpression node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof CastExpression){
+			CastExpression ce = (CastExpression) localStoredTree;
+			// check for an application
+			matcher.match(node, ce);
+		}
+		return false;
+	}
+	
 
 }
