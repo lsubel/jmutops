4ab9e4b8d0ec3e35ff656cb0af5c03c2f42ac95d
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-04 23:29:58 +0200

    Implemented PCD_Matcher and PCD_Visitor

--- src/mutationoperators/pcd/PCD_Matcher.java
+++ src/mutationoperators/pcd/PCD_Matcher.java
@@ -3,10 +3,27 @@ package mutationoperators.pcd;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.MutationOperator;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CastExpression;
+
 public class PCD_Matcher extends BaseASTMatcher {
 
 	public PCD_Matcher(MutationOperator mutop) {
 		super(mutop);
+	}
+
+	@Override
+	public boolean match(CastExpression node, Object other) {
+		ASTNode node2 = (ASTNode) other;
+		
+		// check for the same Expression
+		boolean sameExpression = (node.getExpression().subtreeMatch(defaultMatcher, node2));
+
+		if(sameExpression){
+			this.mutop.found(node, node2);
+		}
+		
+		return false;
 	}
 
 }
--- src/mutationoperators/pcd/PCD_Visitor.java
+++ src/mutationoperators/pcd/PCD_Visitor.java
@@ -3,10 +3,24 @@ package mutationoperators.pcd;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.BaseASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CastExpression;
+
 public class PCD_Visitor extends BaseASTVisitor {
 
 	public PCD_Visitor(BaseASTMatcher matcher) {
 		super(matcher);
 	}
 
+	@Override
+	public boolean visit(CastExpression node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for an application
+		matcher.match(node, localStoredTree);
+		
+		return false;
+	}
+	
 }
