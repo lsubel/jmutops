76ce6254867862469a1b101e852c9877453113c7
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-09 15:48:27 +0200

    Completed ISD_Matcher and ISD_Visitor

--- src/mutationoperators/isd/ISD_Matcher.java
+++ src/mutationoperators/isd/ISD_Matcher.java
@@ -3,10 +3,76 @@ package mutationoperators.isd;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.MutationOperator;
 
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SuperFieldAccess;
+import org.eclipse.jdt.core.dom.SuperMethodInvocation;
+
 public class ISD_Matcher extends BaseASTMatcher {
 
 	public ISD_Matcher(MutationOperator mutop) {
 		super(mutop);
 	}
+
+	@Override
+	public boolean match(SuperFieldAccess node, Object other) {
+		// if the compared AST is no MethodInvocation,
+		// we cannot compare them
+		if(other instanceof FieldAccess){
+			FieldAccess node2 = (FieldAccess) other;
+			
+			// check for the same field name
+			boolean sameFieldName = node.getName().subtreeMatch(defaultMatcher, node2.getName());
+			// check for same quanitifer
+			boolean sameQuantifier = ((node.getQualifier() == null) && node2.getExpression() == null) || (node.getQualifier().subtreeMatch(defaultMatcher, node2.getExpression()));
+			// if all conditions are true, notifiy a match
+			if(sameFieldName && sameQuantifier){
+				this.mutop.found(node, node2);
+			}
+		}
+		else if(other instanceof SimpleName){
+			SimpleName node2 = (SimpleName) other;
+			
+			// check for the same name
+			boolean sameFieldName = node.getName().subtreeMatch(defaultMatcher, node2);
+			// if all conditions are true, notify a match
+			if(sameFieldName){
+				this.mutop.found(node, node2);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean match(SuperMethodInvocation node, Object other) {
+		// if the compared AST is no MethodInvocation,
+		// we cannot compare them
+		if(!(other instanceof MethodInvocation)){
+			return false;
+		}
+		MethodInvocation node2 = (MethodInvocation) other;
+		
+		// check for a same method name
+		boolean sameMethodName = node.getName().subtreeMatch(defaultMatcher, node2.getName());
+		// check that the parameters are equal
+		boolean sameArgumentLength = (node.arguments().size() == node2.arguments().size());
+		// check for the same arguments
+		boolean sameArgument = true;
+		if(sameArgumentLength){
+			sameArgument = this.defaultMatcher.safeSubtreeListMatch(node.arguments(), node2.arguments());
+		}
+		// check for the same quantifier
+		boolean sameQuantifier = ((node.getQualifier() == null) && node2.getExpression() == null) || (node.getQualifier().subtreeMatch(defaultMatcher, node2.getExpression()));
+		
+		// if all conditions are true, notfiy a match
+		if(sameMethodName && sameArgumentLength && sameArgument && sameQuantifier){
+			this.mutop.found(node, node2);
+		}
+		
+		return false;
+	}
+	
+	
 
 }
--- src/mutationoperators/isd/ISD_Visitor.java
+++ src/mutationoperators/isd/ISD_Visitor.java
@@ -3,10 +3,73 @@ package mutationoperators.isd;
 import mutationoperators.BaseASTMatcher;
 import mutationoperators.BaseASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SuperFieldAccess;
+import org.eclipse.jdt.core.dom.SuperMethodInvocation;
+
 public class ISD_Visitor extends BaseASTVisitor {
 
 	public ISD_Visitor(BaseASTMatcher matcher) {
 		super(matcher);
 	}
 
+	@Override
+	public boolean visit(SuperFieldAccess node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof FieldAccess){
+			FieldAccess fa = (FieldAccess) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, fa);
+			
+			// visit the qualifier node
+			visitSubtree(node.getQualifier(), fa.getExpression());
+			
+			// visit the name node
+			visitSubtree(node.getName(), fa.getName());
+		}
+		else if(localStoredTree instanceof SimpleName){
+			SimpleName sn = (SimpleName) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, sn);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(SuperMethodInvocation node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof MethodInvocation){
+			MethodInvocation mi = (MethodInvocation) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, mi);
+			
+			// visit the qualifier node
+			visitSubtree(node.getQualifier(), mi.getExpression());
+			
+			// visit the typeArgument nodes
+			visitSubtrees(node.typeArguments(), mi.typeArguments());
+			
+			// visit the name node
+			visitSubtree(node.getName(), mi.getName());
+			
+			// visit the argument nodes
+			visitSubtrees(node.arguments(), mi.arguments());
+		}
+		return false;
+	}
+
+	
+	
 }
