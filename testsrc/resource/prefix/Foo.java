5c02fda50c927193d9aed5a39e03345a1846b5ea
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-09 16:12:16 +0200

    Clean up

--- testsrc/resource/prefix/Foo.java
+++
@@ -1,9 +0,0 @@
-public class Foo { 
-int a = 0; 
-int b = 4; 
-int c = 42; 
-int d = 21; 
-public int getMax(int arg1, int arg2){if(arg1>arg2){return arg1;}else{return arg2;}} 
-
-void method() { System.out.println(); ++this.a; } 
-};
