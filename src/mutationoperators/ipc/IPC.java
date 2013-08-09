11d84bfa7903e5affde70eed2fc0cbb38f8eaefb
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-09 17:59:24 +0200

    Start to implement IPC Need to add tests

---
+++ src/mutationoperators/ipc/IPC.java
@@ -0,0 +1,30 @@
+package mutationoperators.ipc;
+
+import mutationoperators.MutationOperator;
+import results.JMutOpsEventListenerMulticaster;
+import enums.MutationOperatorCategory;
+import enums.MutationOperatorLevel;
+
+public class IPC extends MutationOperator {
+
+	public IPC() {
+		this(null);
+	}
+	
+	public IPC(JMutOpsEventListenerMulticaster eventListener) {
+		super(eventListener);
+		this.oneAST_visitor = new IPC_Visitor(this);
+	}
+
+	@Override
+	protected void setProperties() {
+		this.mutopproperty.setShortname("IPC");
+		this.mutopproperty.setFullname("Explicit call to a parent's constructor deletion");
+		this.mutopproperty.setDescription("Deletes super constructor calls, causing the default constructor of the parent class to be called.");
+		this.mutopproperty.setLevel(MutationOperatorLevel.METHOD_LEVEL);
+		this.mutopproperty.setCategory(MutationOperatorCategory.METHOD_LEVEL);
+		this.mutopproperty.setCanOneAST();
+		this.mutopproperty.setDelete();
+	}
+
+}
