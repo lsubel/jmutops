b189b09858042ea6328a47534a6476cf774d7f64
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-10 15:01:02 +0200

    Started to implement PNC

---
+++ src/mutationoperators/pnc/PNC_Matcher.java
@@ -0,0 +1,12 @@
+package mutationoperators.pnc;
+
+import mutationoperators.MutationOperator;
+import mutationoperators.TwoASTMatcher;
+
+public class PNC_Matcher extends TwoASTMatcher {
+
+	public PNC_Matcher(MutationOperator mutop) {
+		super(mutop);
+	}
+
+}
---
+++ src/mutationoperators/pnc/PNC_Visitor.java
@@ -0,0 +1,12 @@
+package mutationoperators.pnc;
+
+import mutationoperators.TwoASTMatcher;
+import mutationoperators.TwoASTVisitor;
+
+public class PNC_Visitor extends TwoASTVisitor {
+
+	public PNC_Visitor(TwoASTMatcher matcher) {
+		super(matcher);
+	}
+
+}
