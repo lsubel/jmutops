12342d29d464aa95e1bdb3e28bd3adf8204140fd
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-10 23:48:13 +0200

    Started to implement CFDO Update

---
+++ src/mutationoperators/cfdo/CFDO_Update_Visitor.java
@@ -0,0 +1,46 @@
+package mutationoperators.cfdo;
+
+import mutationoperators.TwoASTMatcher;
+import mutationoperators.TwoASTVisitor;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.ContinueStatement;
+
+public class CFDO_Update_Visitor extends TwoASTVisitor {
+
+	public CFDO_Update_Visitor(TwoASTMatcher matcher) {
+		super(matcher);
+	}
+
+	
+	@Override
+	public boolean visit(BreakStatement node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof BreakStatement){
+			BreakStatement bs = (BreakStatement) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, bs);	
+		}
+		return false;
+	}
+
+	@Override
+	public boolean visit(ContinueStatement node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof ContinueStatement){
+			ContinueStatement cs = (ContinueStatement) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, cs);
+		}
+		return false;
+	}
+}
