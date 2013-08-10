8256c022a270a8d890a867963c25f04e1994ed5f
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-10 20:14:29 +0200

    Implemented CFDO with Delete case

---
+++ src/mutationoperators/cfdo/CFDO_Delete.java
@@ -0,0 +1,30 @@
+package mutationoperators.cfdo;
+
+import mutationoperators.MutationOperator;
+import results.JMutOpsEventListenerMulticaster;
+import enums.MutationOperatorCategory;
+import enums.MutationOperatorLevel;
+
+public class CFDO_Delete extends MutationOperator {
+
+	public CFDO_Delete() {
+		this(null);
+	}
+	
+	public CFDO_Delete(JMutOpsEventListenerMulticaster eventListener) {
+		super(eventListener);
+		this.oneAST_visitor = new CFDO_Delete_Visitor(this);
+	}
+
+	@Override
+	protected void setProperties() {
+		this.mutopproperty.setShortname("CFDO");
+		this.mutopproperty.setFullname("Control-flow disruption operator");
+		this.mutopproperty.setDescription("Disrupting normal control-flow.");
+		this.mutopproperty.setLevel(MutationOperatorLevel.METHOD_LEVEL);
+		this.mutopproperty.setCategory(MutationOperatorCategory.METHOD_LEVEL);
+		this.mutopproperty.setCanOneAST();
+		this.mutopproperty.setDelete();
+	}
+
+}
---
+++ src/mutationoperators/cfdo/CFDO_Delete_Visitor.java
@@ -0,0 +1,30 @@
+package mutationoperators.cfdo;
+
+import mutationoperators.MutationOperator;
+import mutationoperators.OneASTVisitor;
+
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.ContinueStatement;
+
+public class CFDO_Delete_Visitor extends OneASTVisitor {
+
+	public CFDO_Delete_Visitor(MutationOperator mutop) {
+		super(mutop);
+	}
+
+	@Override
+	public boolean visit(BreakStatement node) {
+		// since we match with and without a label, we directly notify one
+		this.mutop.found(node);
+		return true;
+	}
+
+	@Override
+	public boolean visit(ContinueStatement node) {
+		// since we match with and without a label, we directly notify one
+		this.mutop.found(node);
+		return true;
+	}
+	
+	
+}
