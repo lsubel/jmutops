ab93e7f20079f70d5d8279286bd593fbe54b1f40
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-11 13:00:21 +0200

    FFixed a bug in PRV_Matcher occured due to the use of labels

--- src/mutationoperators/prv/PRV_Matcher.java
+++ src/mutationoperators/prv/PRV_Matcher.java
@@ -1,10 +1,11 @@
 package mutationoperators.prv;
 
-import mutationoperators.TwoASTMatcher;
 import mutationoperators.MutationOperator;
+import mutationoperators.TwoASTMatcher;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.QualifiedName;
 import org.eclipse.jdt.core.dom.SimpleName;
 
@@ -98,17 +99,23 @@ public class PRV_Matcher extends TwoASTMatcher {
 		if(other instanceof FieldAccess){
 			FieldAccess node2 = (FieldAccess) other;
 			differentVariables = !(node.subtreeMatch(defaultMatcher, node2.getName()));
-			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+			ITypeBinding firstBinding = node.resolveTypeBinding();
+			ITypeBinding secondBinding = node2.resolveTypeBinding();
+			differentClasses   = (firstBinding != null) && (secondBinding != null)  && !(firstBinding.isEqualTo(secondBinding));
 		}
 		else if(other instanceof QualifiedName){
 			QualifiedName node2 = (QualifiedName) other;
 			differentVariables = !(node.subtreeMatch(defaultMatcher, node2.getName()));
-			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+			ITypeBinding firstBinding = node.resolveTypeBinding();
+			ITypeBinding secondBinding = node2.resolveTypeBinding();
+			differentClasses   = (firstBinding != null) && (secondBinding != null)  && !(firstBinding.isEqualTo(secondBinding));
 		}
 		else if(other instanceof SimpleName){
 			SimpleName node2 = (SimpleName) other;
 			differentVariables = !(node.subtreeMatch(defaultMatcher, node2));
-			differentClasses   = !(node.resolveTypeBinding().isEqualTo(node2.resolveTypeBinding()));
+			ITypeBinding firstBinding = node.resolveTypeBinding();
+			ITypeBinding secondBinding = node2.resolveTypeBinding();
+			differentClasses   = (firstBinding != null) && (secondBinding != null)  && !(firstBinding.isEqualTo(secondBinding));
 		}
 		else{
 			return false;
