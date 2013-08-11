a09a2bdbe3db1c2476c2671983770668da95e768
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-11 13:38:08 +0200

    Added the lastly created method to all tests

--- testsrc/mutationoperators/ACO_Test.java
+++ testsrc/mutationoperators/ACO_Test.java
@@ -39,59 +39,69 @@ public class ACO_Test extends MethodTest {
 	public void testAFRO_MethodNumberChanged1() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("setValue(1);", "setValue(1, 2);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_MethodNumberChanged2() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(); Foo f2 = new Foo(2); setValue(f1, f2);", "Foo f1 = new Foo(); Foo f2 = new Foo(2); setValue(f2);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 
 	@Test
 	public void testAFRO_ConstructorNumberChanged1() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(0, 4);", "Foo f1 = new Foo();");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 
 	@Test
 	public void testAFRO_ConstructorNumberChanged2() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(0);", "Foo f1 = new Foo(6, 1);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_ConstructorReordering1() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(42, 0);", "Foo f1 = new Foo(0, 42);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_MethodReordering1() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(); Foo f2 = new Foo(2); setValue(f1, f2);", "Foo f1 = new Foo(); Foo f2 = new Foo(2); setValue(f2, f1);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_MethodReordering2() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("setValue(2, 1);", "setValue(1, 2);");
 		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_NoMatching1() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("int i = getZero();", "int i = getValue();");
 		assertEquals(0, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_NoMatching2() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(); Foo f2 = new Foo(2); ", "Foo f1 = new Foo(); Foo f2 = new Foo(f1); ");
 		assertEquals(0, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 	
 	@Test
 	public void testAFRO_NoMatching3() {
 		HashMap<MutationOperator, Integer> resultMap = compareMatches("Foo f1 = new Foo(); setValue(f1); ", "Foo f1 = new Foo(); setValue(1); ");
 		assertEquals(0, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
 	}
 }
