c860bc05e9482c18e449df24520b9e0968e07870
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-13 13:23:46 +0200

    Implemented EXCO Move case

--- src/mutationoperators/methodlevel/exco/EXCO_Move_Matcher.java
+++ src/mutationoperators/methodlevel/exco/EXCO_Move_Matcher.java
@@ -3,11 +3,52 @@ package mutationoperators.methodlevel.exco;
 import mutationoperators.MutationOperator;
 import mutationoperators.TwoASTMatcher;
 
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.TryStatement;
+
 public class EXCO_Move_Matcher extends TwoASTMatcher {
 
 	public EXCO_Move_Matcher(MutationOperator mutop) {
 		super(mutop);
-		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public boolean match(CatchClause node, Object other) {
+		// if the compared AST is no CatchClause,
+		// we cannot compare them
+		if(!(other instanceof CatchClause)){
+			return false;
+		}
+		CatchClause node2 = (CatchClause) other;
+		
+		// get the parent node of both elements
+		boolean haveTryParent = (node.getParent() instanceof TryStatement) && (node2.getParent() instanceof TryStatement);
+		if(!haveTryParent){
+			return false;
+		}
+		TryStatement try1 = (TryStatement) node.getParent();
+		TryStatement try2 = (TryStatement) node2.getParent();
+		
+		// check if both catch clauses are the same;
+		// implicit due to the Move from ChangeDistiller
+		boolean sameCatch = node.subtreeMatch(defaultMatcher, node2);
+		
+		// check if both try statements have the same body
+		boolean sameParentBody = (try1.getBody().subtreeMatch(defaultMatcher, try2.getBody()));
+		
+		// check that both catches have different indices in their catch clause block
+		// (=> a move within the same block exists)
+		boolean differentIndices = (try1.catchClauses().indexOf(node) != try2.catchClauses().indexOf(node2));
+		
+		// if all of these conditions are true, we detected a matching
+		if(sameCatch && sameParentBody && differentIndices){
+			this.mutop.found(node, node2);
+			return true;
+		}
+		
+		return false;
+	}
+
+	
+	
 }
--- src/mutationoperators/methodlevel/exco/EXCO_Move_Visitor.java
+++ src/mutationoperators/methodlevel/exco/EXCO_Move_Visitor.java
@@ -3,11 +3,36 @@ package mutationoperators.methodlevel.exco;
 import mutationoperators.TwoASTMatcher;
 import mutationoperators.TwoASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CatchClause;
+
 public class EXCO_Move_Visitor extends TwoASTVisitor {
 
 	public EXCO_Move_Visitor(TwoASTMatcher matcher) {
 		super(matcher);
-		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public boolean visit(CatchClause node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof CatchClause){
+			CatchClause cc = (CatchClause) localStoredTree;
+		
+			// check for a match
+			this.matcher.match(node, cc);
+			
+			// visit the FormalParameter node
+			visitSubtree(node.getException(), cc.getException());
+			
+			// visit the block node
+			visitSubtree(node.getBody(), cc.getBody());
+		}
+		return false;
+	}
+	
+	
 
 }
