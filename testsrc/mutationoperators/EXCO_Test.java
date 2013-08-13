e1fa3aae4824614d0c1ecba20765be65e38f8883
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-13 12:37:15 +0200

    Created some initial tests for EXCO

---
+++ testsrc/mutationoperators/EXCO_Test.java
@@ -0,0 +1,62 @@
+package mutationoperators;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import utils.MethodTest;
+
+public class EXCO_Test extends MethodTest {
+
+	MutationOperator mutop;
+	
+	@Override
+	protected void initializeMutationOperatorsToTest() {
+		this.mutop = null; // TODO: Change!
+		this.addMutationOperatorToTest(mutop);
+	}
+
+	@Override
+	protected String getOtherClassContent() {
+		return 	"int a = 1; " +
+				"int b = 2; ";
+	}
+	
+	@Test
+	public void testEXCO_addNewCatch1(){
+		String pre 	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
+		String post	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
+		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
+		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
+	}
+	
+	@Test
+	public void testEXCO_removeCatch1(){
+		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
+		String post = "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
+		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
+		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
+	}
+	
+	@Test
+	public void testEXCO_reorderCatch1(){
+		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
+		String post = "try {Object o = null;o.equals(null);} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
+		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
+		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
+	}
+	
+	@Test
+	public void testEXCO_replaceExceptionType1(){
+		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
+		String post = "try {Object o = null;o.equals(null);} catch (Exception e) {System.out.println(\"NullPointerException\");}";
+		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
+		assertEquals(1, resultMap.get(mutop).intValue());
+		checkOtherMutationOperators(resultMap, mutop);
+	}
+}
