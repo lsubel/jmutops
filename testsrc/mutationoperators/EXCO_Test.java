d5bd2567368162b4a915e5c5cd27ff04fad2c4fd
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-08-13 12:58:25 +0200

    Updated test case for EXCO

--- testsrc/mutationoperators/EXCO_Test.java
+++ testsrc/mutationoperators/EXCO_Test.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 
 import mutationoperator.MutationOperator;
+import mutationoperator.methodlevel.exco.EXCO_Delete;
+import mutationoperator.methodlevel.exco.EXCO_Insert;
+import mutationoperator.methodlevel.exco.EXCO_Move;
+import mutationoperator.methodlevel.exco.EXCO_Update;
 
 import org.junit.Test;
 
@@ -12,12 +16,21 @@ import utils.MethodTest;
 
 public class EXCO_Test extends MethodTest {
 
-	MutationOperator mutop;
+	private MutationOperator mutop_insert;
+	private MutationOperator mutop_delete;
+	private MutationOperator mutop_update;
+	private MutationOperator mutop_move;
 	
 	@Override
 	protected void initializeMutationOperatorsToTest() {
-		this.mutop = null; // TODO: Change!
-		this.addMutationOperatorToTest(mutop);
+		this.mutop_insert = new EXCO_Insert();
+		this.addMutationOperatorToTest(mutop_insert);
+		this.mutop_delete = new EXCO_Delete();
+		this.addMutationOperatorToTest(mutop_insert);
+		this.mutop_update = new EXCO_Update();
+		this.addMutationOperatorToTest(mutop_insert);
+		this.mutop_move = new EXCO_Move();
+		this.addMutationOperatorToTest(mutop_insert);
 	}
 
 	@Override
@@ -31,8 +44,8 @@ public class EXCO_Test extends MethodTest {
 		String pre 	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
 		String post	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
 		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, resultMap.get(mutop).intValue());
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, resultMap.get(mutop_insert).intValue());
+		checkOtherMutationOperators(resultMap, mutop_insert);
 	}
 	
 	@Test
@@ -40,8 +53,8 @@ public class EXCO_Test extends MethodTest {
 		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
 		String post = "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
 		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, resultMap.get(mutop).intValue());
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, resultMap.get(mutop_delete).intValue());
+		checkOtherMutationOperators(resultMap, mutop_delete);
 	}
 	
 	@Test
@@ -49,8 +62,8 @@ public class EXCO_Test extends MethodTest {
 		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");}";
 		String post = "try {Object o = null;o.equals(null);} catch (IllegalStateException e){System.out.println(\"IllegalStateException\");} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
 		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, resultMap.get(mutop).intValue());
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, resultMap.get(mutop_move).intValue());
+		checkOtherMutationOperators(resultMap, mutop_move);
 	}
 	
 	@Test
@@ -58,7 +71,7 @@ public class EXCO_Test extends MethodTest {
 		String pre	= "try {Object o = null;o.equals(null);} catch (NullPointerException e) {System.out.println(\"NullPointerException\");}";
 		String post = "try {Object o = null;o.equals(null);} catch (Exception e) {System.out.println(\"NullPointerException\");}";
 		HashMap<MutationOperator, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, resultMap.get(mutop).intValue());
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, resultMap.get(mutop_update).intValue());
+		checkOtherMutationOperators(resultMap, mutop_update);
 	}
 }
