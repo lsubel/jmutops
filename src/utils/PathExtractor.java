adc40baa82612b5b051d6350242fd3a08ddedec3
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-10 13:39:36 +0200

    Added comments to PathExtractor Refactorized methods in PathExtractor Reduced redundant code Added Generic types to List objects

--- src/utils/PathExtractor.java
+++ src/utils/PathExtractor.java
@@ -68,6 +68,7 @@ import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.SimpleType;
 import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.SuperConstructorInvocation;
 import org.eclipse.jdt.core.dom.SuperFieldAccess;
@@ -91,113 +92,154 @@ import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.WhileStatement;
 import org.eclipse.jdt.core.dom.WildcardType;
 
+/**
+ * ASTVisitor. Generated a list of {@link Integer} values 
+ * which represents the way from a ASTNode up to the corresponding MethodDecleration. <p>
+ * Used as a workaround to detected Insert/Delete from ChangeDistiller at the exact same position. <p>
+ * @author Lukas Subel
+ *
+ */
 public class PathExtractor extends ASTVisitor {
 
+	/**
+	 * Public static method which generates a list of {@link Integer} 
+	 * from the argument {@code node} up to the corresponding MethodDecleration.
+	 * @param node The starting ASTNode.
+	 * @return A Stack<Integer> which represents the path.
+	 */
 	public static Stack<Integer> calculatePathToMethod(ASTNode node) {
-		
-		PathExtractor pe = new PathExtractor(node);
-		node.getParent().accept(pe);
-		
-		return pe.stack;
+		// calculate the way to from the ASTNode node to its MethodDecleration
+		extractor.resetFields();
+		extractor.setField(node);
+		node.getParent().accept(extractor);
+		return extractor.getWayStack();
 	}
 	
-	private Stack<Integer> stack = new Stack<Integer>();
+	private static PathExtractor extractor = new PathExtractor();
 	
 	private ASTMatcher defaultMatcher = new ASTMatcher();
 	
-	private ASTNode child = null;
+	private Stack<Integer> stack;
 	
-	public PathExtractor(ASTNode node) {
-		child = node;
+	private ASTNode child;
+
+	/**
+	 * Default constructor.
+	 */
+	public PathExtractor() {
+		resetFields();
 	}
 
-	@Override
-	public boolean visit(AssertStatement node) {
-		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
-		}
-		else {
-			stack.push(new Integer(-1));
-			this.child = node;	
-		}
+	public void setField(ASTNode child) {
+		this.child = child;
+	}
+	
+	public void resetFields() {
+		this.stack = new Stack<Integer>();
+		this.child = null;
+	}
+
+	public Stack<Integer> getWayStack() {
+		return this.stack;
+	}
+	
+	//////////////////////////////////////////////////////////////////
+	///	ASTVisitor visit() methods
+	//////////////////////////////////////////////////////////////////
+	
+	public void tryToWalkUpwards(ASTNode node) {
 		if(node.getParent() != null) {
 			node.getParent().accept(this);
 		}
+	}
+	
+	public void updateStackAndChild(ASTNode node, Integer value) {
+		this.stack.push(value);
+		this.child = node;
+	}
+	
+	@Override
+	public boolean visit(AssertStatement node) {
+		// if the expression of the assert statements is matching
+		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
+			updateStackAndChild(node, 0);
+		}
+		// otherwise
+		else {
+			updateStackAndChild(node, -1);	
+		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 	
 	@Override
 	public boolean visit(Block node) {
-		List stmts = node.statements();
+		// extract list of containing statements
+		List<Statement> stmts = node.statements();
+		// search for the index of the matching subnode
 		boolean found = false;
 		for(int i = 0; i < stmts.size(); i++) {
 			if(this.child.subtreeMatch(defaultMatcher, stmts.get(i))) {
-				stack.push(new Integer(i));
-				this.child = node;
+				updateStackAndChild(node, i);
 				found = true;
 				break;
 			}
 		}
 		if(!found) {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(BreakStatement node) {
+		// check for a matching in the labe
 		if(this.child.subtreeMatch(defaultMatcher, node.getLabel())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 	
 	@Override
 	public boolean visit(ContinueStatement node) {
+		// check if the labels are matching
 		if(this.child.subtreeMatch(defaultMatcher, node.getLabel())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else{
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 	
 	@Override
 	public boolean visit(DoStatement node) {
+		// check if the expression (=> while(EXPR) ) are matching
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check if the body is the same
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
@@ -208,262 +250,251 @@ public class PathExtractor extends ASTVisitor {
 
 	@Override
 	public boolean visit(EnhancedForStatement node) { 
+		// check if the expressions (=> for(X x: EXPR) ) are the same
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check if the body is matching
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(ExpressionStatement node) {
+		// check if the expression is matching
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(ForStatement node) {
+		// check if the expression is matching
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check if the body is matching
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(IfStatement node) {
+		// check for a matching expression
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check for a matching then statement
 		else if(this.child.subtreeMatch(defaultMatcher, node.getThenStatement())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// check for a matching else statement
 		else if(this.child.subtreeMatch(defaultMatcher, node.getElseStatement())) {
-			stack.push(2);
-			this.child = node;
+			updateStackAndChild(node, 2);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(LabeledStatement node) {
+		// check for a matching label
 		if(this.child.subtreeMatch(defaultMatcher, node.getLabel())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check for a matching body
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(ReturnStatement node) {
+		// check for a matching expression in the returning part
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(SwitchStatement node) {
 		boolean found = false;
+		// check for a matching expression (=> switch(EXPR) )
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node; 
+			updateStackAndChild(node, 0);
 			found = true;
 		}
 		else {
-			List stmts = node.statements();
+			// extract the list of containing statements
+			List<Statement> stmts = node.statements();
+			// search for the index of the matching subnode
 			for(int i = 0; i < stmts.size(); i++) {
 				if(this.child.subtreeMatch(defaultMatcher, stmts.get(i))) {
-					stack.push(new Integer(1 + i));
-					this.child = node;
+					updateStackAndChild(node, 1 + i);
 					found = true;
 				}
 			}
 		}
+		// otherwise
 		if(!found) {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(SynchronizedStatement node) {
+		// check for a matching expression
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check for a matching body
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(ThrowStatement node) {
+		// check for a matching expression
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(TryStatement node) {
 		boolean found = false;
+		// check for a matching body
 		if(this.child.subtreeMatch(defaultMatcher, node.getBody() )) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 			found = true;
 		}
+		// check for a matching finally expression
 		else if(this.child.subtreeMatch(defaultMatcher, node.getFinally())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 			found = true;
 		}
-		else{ 
-			List stmts = node.catchClauses();
+		else{
+			// extract a list if all catch clauses
+			List<CatchClause> stmts = node.catchClauses();
+			// search for the index of the matching subnode
 			for(int i = 0; i < stmts.size(); i++) {
 				if(this.child.subtreeMatch(defaultMatcher, stmts.get(i))) {
-					stack.push(new Integer(2 + i));
-					this.child = node;
+					updateStackAndChild(node, 2 + i);
 					found = true;
 					break;
 				}
 			}
 		}
+		// otherwise
 		if(!found) {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(TypeDeclarationStatement node) {
+		// check for a matching decleration
 		if(this.child.subtreeMatch(defaultMatcher, node.getDeclaration())) {
-			stack.push(0);
-			this.child = node;
-			
+			updateStackAndChild(node, 0);
 		}
+		// otherwise
 		else{
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(VariableDeclarationStatement node) {
+		// otherwise
 		stack.push(new Integer(-1));
 		this.child = node;
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
 	@Override
 	public boolean visit(WhileStatement node) {
+		// check for a matching expression
 		if(this.child.subtreeMatch(defaultMatcher, node.getExpression())) {
-			stack.push(0);
-			this.child = node;
+			updateStackAndChild(node, 0);
 		}
+		// check for a matching body
 		else if(this.child.subtreeMatch(defaultMatcher, node.getBody())) {
-			stack.push(1);
-			this.child = node;
+			updateStackAndChild(node, 1);
 		}
+		// otherwise
 		else {
-			stack.push(new Integer(-1));
-			this.child = node;
+			updateStackAndChild(node, -1);
 		}
-		if(node.getParent() != null) {
-			node.getParent().accept(this);
-		}
+		// check if we can walk upwards
+		tryToWalkUpwards(node);
 		return false;
 	}
 
