5676045ca0e67acc9ccb52e554838ac7aba989bd
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-11 21:47:29 +0200

    Added check for final modifier Added another case for VariableDeclarationFragment

--- src/mutationoperators/methodlevel/icm/ICM_Matcher.java
+++ src/mutationoperators/methodlevel/icm/ICM_Matcher.java
@@ -6,8 +6,7 @@ import mutationoperators.TwoASTMatcher;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.BooleanLiteral;
 import org.eclipse.jdt.core.dom.Expression;
-
-import utils.LiteralValueExtractor;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 public class ICM_Matcher extends TwoASTMatcher {
 
@@ -34,135 +33,310 @@ public class ICM_Matcher extends TwoASTMatcher {
 			Expression node_right_expr  = node.getRightHandSide();
 			Expression node2_right_expr = node2.getRightHandSide();
 			
-			
 			// 1) boolean
 			if(node_left_type.equals("boolean")) {
-				// check some conditions
-				boolean correctLeftNode = (node_right_expr instanceof BooleanLiteral);
-				boolean correctRightNode = (node2_right_expr instanceof BooleanLiteral);
-				
-				if(correctLeftNode && correctRightNode) {
-					boolean differentValues = (((BooleanLiteral) node_right_expr).booleanValue() ^ ((BooleanLiteral) node2_right_expr).booleanValue());
-					
-					// if all conditions are valid, notify a matching
-					if(differentValues) {
-						this.mutop.found(node, node2);
-					}
+				// check for a matching
+				if(checkBooleanMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
 				}
-				
 			}
 			// 2) int, byte, short
 			else if(node_left_type.equals("int") || node_left_type.equals("byte") || node_left_type.equals("short") ) {
-				// get the right handed value
-				int node_value = 0;
-				int node2_value = 0;
-				
-				// try to calculcate the value for the literal node
-				try {
-					node_value = (int) LiteralValueExtractor.getValue(node_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				try {
-					node2_value = (int) LiteralValueExtractor.getValue(node2_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				
-				// check for any of the defined transformation
-				boolean OneToZero = ((node_value == 1) && (node2_value == 0));
-				boolean MinusOneToOne = ((node_value == -1) && (node2_value == 1));
-				boolean FiveToMinusOne = ((node_value == 5) && (node2_value == -1));
-				boolean OthersToIncrement = ((node_value != 1) && (node_value != -1) && (node_value != 5) && ((node_value + 1) == node2_value));
-				
-				// if all conditions are valid, notify a matching
-				if(OneToZero || MinusOneToOne || FiveToMinusOne || OthersToIncrement) {
+				// check for a matching
+				if(checkIntByteShortMatch(node_right_expr, node2_right_expr)) {
 					this.mutop.found(node, node2);
 				}
 			}
 			else if(node_left_type.equals("long")) {
-				// get the right handed value
-				long node_value = 0;
-				long node2_value = 0;
-				
-				// try to calculcate the value for the literal node
-				try {
-					node_value = (long) LiteralValueExtractor.getValue(node_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				try {
-					node2_value = (long) LiteralValueExtractor.getValue(node2_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				
-				// check for any of the defined transformation
-				boolean OneToZero = ((node_value == 1) && (node2_value == 0));
-				boolean OthersToIncrement = ((node_value != 1) && ((node_value + 1) == node2_value));
-				
-				// if all conditions are valid, notify a matching
-				if(OneToZero || OthersToIncrement) {
+				// check for a matching
+				if(checkLongMatch(node_right_expr, node2_right_expr)) {
 					this.mutop.found(node, node2);
 				}
 			}
 			else if(node_left_type.equals("float")) {
-				// get the right handed value
-				float node_value = 0;
-				float node2_value = 0;
-				
-				// try to calculcate the value for the literal node
-				try {
-					node_value = (float) LiteralValueExtractor.getValue(node_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				try {
-					node2_value = (float) LiteralValueExtractor.getValue(node2_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				
-				// check for any of the defined transformation
-				boolean OneToZero = ((node_value == 1.0) && (node2_value == 0.0));
-				boolean TwoToZero = ((node_value == 2.0) && (node2_value == 0.0));
-				boolean OthersToIncrement = ((node_value != 1.0) && (node_value != 2.0) && (node2_value == 1.0));
-				
-				// if all conditions are valid, notify a matching
-				if(OneToZero || TwoToZero || OthersToIncrement) {
+				// check for a matching
+				if(checkFloatMatch(node_right_expr, node2_right_expr)) {
 					this.mutop.found(node, node2);
 				}
 			}
 			else if(node_left_type.equals("double")) {
-				// get the right handed value
-				double node_value = 0;
-				double node2_value = 0;
-				
-				// try to calculcate the value for the literal node
-				try {
-					node_value = LiteralValueExtractor.getValue(node_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				try {
-					node2_value = LiteralValueExtractor.getValue(node2_right_expr);
-				} catch (Exception e) {
-					return false;
-				}
-				
-				// check for any of the defined transformation
-				boolean OneToZero = ((node_value == 1.0) && (node2_value == 0.0));
-				boolean OthersToIncrement = ((node_value != 1.0) && (node2_value == 1.0));
-				
-				// if all conditions are valid, notify a matching
-				if(OneToZero || OthersToIncrement) {
+				// check for a matching
+				if(checkDoubleMatch(node_right_expr, node2_right_expr)) {
 					this.mutop.found(node, node2);
 				}
 			}
+		}		
+		return false;
+	}
+	
+	@Override
+	public boolean match(VariableDeclarationFragment node, Object other) {
+		// cast the postfix object
+		if(!(other instanceof VariableDeclarationFragment)) {
+			return false;
 		}
+		VariableDeclarationFragment node2 = (VariableDeclarationFragment) other;
+				
+		// resolve the left side type
+		String node_left_type  = node.getName().resolveTypeBinding().getName();
+		String node2_left_type = node2.getName().resolveTypeBinding().getName();
+		boolean sameTypeName = node_left_type.equals(node2_left_type);
 		
-		
+		if(sameTypeName) {
+			// depending on the type if the left side,
+			// test for the specified mutations
+			Expression node_right_expr  = node.getInitializer();
+			Expression node2_right_expr = node2.getInitializer();
+			
+			// check if both expressions have initializer;
+			// in this case abort
+			if((node_right_expr == null) || (node2_right_expr == null)) {
+				return false;
+			}
+			
+			// 1) boolean
+			if(node_left_type.equals("boolean")) {
+				// check for a matching
+				if(checkBooleanMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
+				}
+			}
+			// 2) int, byte, short
+			else if(node_left_type.equals("int") || node_left_type.equals("byte") || node_left_type.equals("short") ) {
+				// check for a matching
+				if(checkIntByteShortMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
+				}
+			}
+			else if(node_left_type.equals("long")) {
+				// check for a matching
+				if(checkLongMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
+				}
+			}
+			else if(node_left_type.equals("float")) {
+				// check for a matching
+				if(checkFloatMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
+				}
+			}
+			else if(node_left_type.equals("double")) {
+				// check for a matching
+				if(checkDoubleMatch(node_right_expr, node2_right_expr)) {
+					this.mutop.found(node, node2);
+				}
+			}
+		}		
 		return false;
 	}
 
+
+	//////////////////////////////////////////
+	/// private methods
+	//////////////////////////////////////////
+	
+	
+
+	private boolean checkBooleanMatch(Expression node_right_expr, Expression node2_right_expr) {
+		// check some conditions
+		boolean correctLeftNode = (node_right_expr instanceof BooleanLiteral);
+		boolean correctRightNode = (node2_right_expr instanceof BooleanLiteral);
+		
+		if(correctLeftNode && correctRightNode) {
+			boolean differentValues = (((BooleanLiteral) node_right_expr).booleanValue() ^ ((BooleanLiteral) node2_right_expr).booleanValue());
+		
+			return differentValues;
+			
+		}
+		return false;
+	}
+
+	private boolean checkIntByteShortMatch(Expression node_right_expr,
+			Expression node2_right_expr) {
+		// get the right handed value
+		int node_value = 0;
+		int node2_value = 0;
+		
+		// try to calculcate the value for the literal node
+		Object node_obj_value = node_right_expr.resolveConstantExpressionValue();
+		Object node2_obj_value = node2_right_expr.resolveConstantExpressionValue();
+		
+		boolean node_valid_object = ((node_obj_value instanceof Integer) || (node_obj_value instanceof Byte) || (node_obj_value instanceof Short));
+		boolean node2_valid_object = ((node2_obj_value instanceof Integer) || (node2_obj_value instanceof Byte) || (node2_obj_value instanceof Short));
+		boolean compatibleType = node_right_expr.resolveTypeBinding().isCastCompatible(node2_right_expr.resolveTypeBinding());
+		
+		if(node_valid_object && node2_valid_object && compatibleType) {
+			// extract the values
+			if(node_obj_value instanceof Integer) {
+				node_value	= ((Integer) node_obj_value).intValue(); 
+			}
+			else if(node_obj_value instanceof Byte) {
+				node_value	= ((Byte) node_obj_value).byteValue();
+			}
+			else if(node_obj_value instanceof Short) {
+				node_value	= ((Short) node_obj_value).shortValue();
+			}
+			else {
+				throw new IllegalStateException("This cannot happen!");
+			}
+			
+			if(node2_obj_value instanceof Integer) {
+				node2_value	= ((Integer) node2_obj_value).intValue(); 
+			}
+			else if(node2_obj_value instanceof Byte) {
+				node2_value	= ((Byte) node2_obj_value).byteValue();
+			}
+			else if(node2_obj_value instanceof Short) {
+				node2_value	= ((Short) node2_obj_value).shortValue();
+			}
+			else {
+				throw new IllegalStateException("This cannot happen!");
+			}
+		}
+		else {
+			return false;
+		}
+
+		// check for any of the defined transformation
+		boolean OneToZero = ((node_value == 1) && (node2_value == 0));
+		boolean MinusOneToOne = ((node_value == -1) && (node2_value == 1));
+		boolean FiveToMinusOne = ((node_value == 5) && (node2_value == -1));
+		boolean OthersToIncrement = ((node_value != 1) && (node_value != -1) && (node_value != 5) && ((node_value + 1) == node2_value));
+		
+		// if all conditions are valid, notify a matching
+		if(OneToZero || MinusOneToOne || FiveToMinusOne || OthersToIncrement) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private boolean checkLongMatch(Expression node_right_expr,
+			Expression node2_right_expr) {
+		// get the right handed value
+		long node_value = 0;
+		long node2_value = 0;
+		
+		// try to calculcate the value for the literal node
+		Object node_obj_value = node_right_expr.resolveConstantExpressionValue();
+		Object node2_obj_value = node2_right_expr.resolveConstantExpressionValue();
+		
+		boolean node_valid_object = (node_obj_value instanceof Long) || (node_obj_value instanceof Integer);
+		boolean node2_valid_object = (node2_obj_value instanceof Long) || (node2_obj_value instanceof Integer);
+		boolean sameType = node_obj_value.getClass().equals(node2_obj_value.getClass());
+		
+		if(node_valid_object && node2_valid_object && sameType) {
+			// extract the values
+			if(node_obj_value instanceof Long) {
+				node_value	= ((Long) node_obj_value).longValue();
+				node2_value = ((Long) node2_obj_value).longValue();
+			}
+			else if(node_obj_value instanceof Integer) {
+				node_value	= ((Integer) node_obj_value).longValue();
+				node2_value = ((Integer) node2_obj_value).longValue();
+			}
+			else {
+				throw new IllegalStateException("This cannot happen!");
+			}
+		}
+		else {
+			return false;
+		}
+		
+		// check for any of the defined transformation
+		boolean OneToZero = ((node_value == 1) && (node2_value == 0));
+		boolean OthersToIncrement = ((node_value != 1) && ((node_value + 1) == node2_value));
+		
+		// if all conditions are valid, notify a matching
+		if(OneToZero || OthersToIncrement) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean checkDoubleMatch(Expression node_right_expr,
+			Expression node2_right_expr) {
+		// get the right handed value
+		double node_value = 0;
+		double node2_value = 0;
+		
+		// try to calculcate the value for the literal node
+		Object node_obj_value = node_right_expr.resolveConstantExpressionValue();
+		Object node2_obj_value = node2_right_expr.resolveConstantExpressionValue();
+		
+		boolean node_valid_object = (node_obj_value instanceof Double);
+		boolean node2_valid_object = (node2_obj_value instanceof Double);
+		boolean sameType = node_obj_value.getClass().equals(node2_obj_value.getClass());
+		
+		if(node_valid_object && node2_valid_object && sameType) {
+			// extract the values
+			node_value	= ((Double) node_obj_value).doubleValue();
+			node2_value = ((Double) node2_obj_value).doubleValue();
+		}
+		else {
+			return false;
+		}
+		
+		// check for any of the defined transformation
+		boolean OneToZero = ((node_value == 1.0) && (node2_value == 0.0));
+		boolean OthersToIncrement = ((node_value != 1.0) && (node2_value == 1.0));
+		
+		// if all conditions are valid, notify a matching
+		if(OneToZero || OthersToIncrement) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkFloatMatch(Expression node_right_expr,
+			Expression node2_right_expr) {
+		// get the right handed value
+		double node_value = 0;
+		double node2_value = 0;
+		
+		// try to calculcate the value for the literal node
+		Object node_obj_value = node_right_expr.resolveConstantExpressionValue();
+		Object node2_obj_value = node2_right_expr.resolveConstantExpressionValue();
+		
+		boolean node_valid_object = (node_obj_value instanceof Float) || ((node_obj_value instanceof Double));
+		boolean node2_valid_object = (node2_obj_value instanceof Float) || ((node2_obj_value instanceof Double));
+		boolean compatibleType = node_right_expr.resolveTypeBinding().isCastCompatible(node2_right_expr.resolveTypeBinding());
+		
+		if(node_valid_object && node2_valid_object && compatibleType) {
+			if(node_obj_value instanceof Float) {
+				node_value	= ((Float) node_obj_value).floatValue();
+			}
+			else if(node_obj_value instanceof Double) {
+				node_value	= ((Double) node_obj_value).doubleValue();
+			}
+			else {
+				throw new IllegalStateException("This cannot happen!");
+			}
+			
+			if(node2_obj_value instanceof Float) {
+				node2_value	= ((Float) node2_obj_value).floatValue();
+			}
+			else if(node2_obj_value instanceof Double) {
+				node2_value	= ((Double) node2_obj_value).doubleValue();
+			}
+			else {
+				throw new IllegalStateException("This cannot happen!");
+			}
+		}
+		else {
+			return false;
+		}
+		
+		// check for any of the defined transformation
+		boolean OneToZero = ((node_value == 1.0) && (node2_value == 0.0));
+		boolean TwoToZero = ((node_value == 2.0) && (node2_value == 0.0));
+		boolean OthersToIncrement = ((node_value != 1.0) && (node_value != 2.0) && (node2_value == 1.0));
+		
+		// if all conditions are valid, notify a matching
+		if(OneToZero || TwoToZero || OthersToIncrement) {
+			return true;
+		}
+		return false;
+	}
+	
 }
--- src/mutationoperators/methodlevel/icm/ICM_Visitor.java
+++ src/mutationoperators/methodlevel/icm/ICM_Visitor.java
@@ -6,6 +6,8 @@ import mutationoperators.TwoASTVisitor;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
 public class ICM_Visitor extends TwoASTVisitor {
 
@@ -28,19 +30,58 @@ public class ICM_Visitor extends TwoASTVisitor {
 			ITypeBinding type_postfix_right = assign.getRightHandSide().resolveTypeBinding();
 			
 			// check for some preconditions
-			boolean leftIsPrimitive = (type_prefix_left.isPrimitive() && type_postfix_left.isPrimitive());
-			boolean rightIsPrimitive = (type_prefix_right.isPrimitive() && type_postfix_right.isPrimitive());
-			boolean sameLeftSide = node.getLeftHandSide().subtreeMatch(defaultMatcher, assign.getLeftHandSide());
-			boolean sameLeftType = type_prefix_left.isEqualTo(type_postfix_left);
-			boolean sameRightType = type_prefix_right.isEqualTo(type_postfix_right) || (type_postfix_right.isCastCompatible(type_postfix_left));
+			// TODO resolve Binding for left side
+			boolean isNotFinal       	= (!Modifier.isFinal(type_prefix_left.getModifiers()) && !Modifier.isFinal(type_postfix_left.getModifiers()));
+			boolean leftIsPrimitive  	= (type_prefix_left.isPrimitive() && type_postfix_left.isPrimitive());
+			boolean rightIsPrimitive 	= (type_prefix_right.isPrimitive() && type_postfix_right.isPrimitive());
+			boolean sameLeftSide 		= node.getLeftHandSide().subtreeMatch(defaultMatcher, assign.getLeftHandSide());
+			boolean sameLeftType 		= type_prefix_left.isEqualTo(type_postfix_left);
+			boolean sameRightType 		= type_prefix_right.isEqualTo(type_postfix_right) || (type_postfix_right.isCastCompatible(type_postfix_left));
 			
 			// if all conditions are valid,
 			// try to match it
-			if(leftIsPrimitive && rightIsPrimitive && sameLeftSide && sameLeftType && sameRightType) {
+			if(isNotFinal && leftIsPrimitive && rightIsPrimitive && sameLeftSide && sameLeftType && sameRightType) {
 				this.matcher.match(node, assign);
 			}
 		}
-		
 		return false;
 	}
+
+	@Override
+	public boolean visit(VariableDeclarationFragment node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+		
+		// check for same node type in parallel tree
+		if(localStoredTree instanceof VariableDeclarationFragment){
+			VariableDeclarationFragment vdf = (VariableDeclarationFragment) localStoredTree;
+			
+			// if there is no initializer, abort
+			if((node.getInitializer() == null) || (vdf.getInitializer() == null)) {
+				return false;
+			}
+			
+			ITypeBinding type_prefix_left 	= node.getName().resolveTypeBinding();
+			ITypeBinding type_prefix_right 	= node.getInitializer().resolveTypeBinding();
+			ITypeBinding type_postfix_left 	= vdf.getName().resolveTypeBinding();
+			ITypeBinding type_postfix_right = vdf.getInitializer().resolveTypeBinding();
+			
+			// check for some preconditions
+			boolean isNotFinal       	= (!Modifier.isFinal(node.getName().resolveBinding().getModifiers()) && !Modifier.isFinal(node.getName().resolveBinding().getModifiers()));
+			boolean leftIsPrimitive  	= (type_prefix_left.isPrimitive() && type_postfix_left.isPrimitive());
+			boolean rightIsPrimitive 	= (type_prefix_right.isPrimitive() && type_postfix_right.isPrimitive());
+			boolean sameLeftSide 		= node.getName().subtreeMatch(defaultMatcher, vdf.getName());
+			boolean sameLeftType 		= type_prefix_left.isEqualTo(type_postfix_left);
+			boolean sameRightType 		= type_prefix_right.isEqualTo(type_postfix_right) || (type_postfix_right.isCastCompatible(type_postfix_left));
+			
+			// if all conditions are valid,
+			// try to match it
+			if(isNotFinal && leftIsPrimitive && rightIsPrimitive && sameLeftSide && sameLeftType && sameRightType) {
+				this.matcher.match(node, vdf);
+			}
+		}
+		return false;
+	}
+	
+	
 }
