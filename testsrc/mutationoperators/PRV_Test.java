a955c764b34b7decc849432df684e473c8d190c5
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-12 22:19:30 +0200

    Updated PRV_Tests

--- testsrc/mutationoperators/PRV_Test.java
+++ testsrc/mutationoperators/PRV_Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 
 import mutationoperators.methodlevel.prv.PRV;
+import mutationoperators.methodlevel.vro.VRO;
 
 import org.junit.Test;
 
@@ -12,12 +13,15 @@ import utils.MethodTest;
 
 public class PRV_Test extends MethodTest {
 
-	MutationOperator mutop;
+	MutationOperator mutop_prv;
+	MutationOperator mutop_vro;
 	
 	@Override
 	protected void initializeMutationOperatorsToTest() {
-		this.mutop = new PRV();
-		this.addMutationOperatorToTest(mutop);
+		this.mutop_prv = new PRV();
+		this.addMutationOperatorToTest(mutop_prv);
+		this.mutop_vro = new VRO();
+		this.addMutationOperatorToTest(mutop_vro);
 	}
 
 	@Override
@@ -45,8 +49,9 @@ public class PRV_Test extends MethodTest {
 		String pre 	= "Object obj; String s = \"Hello\"; Integer i = new Integer(4); obj = s; ";
 		String post = "Object obj; String s = \"Hello\"; Integer i = new Integer(4); obj = i; ";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_prv));
+		assertEquals(0, getApplicationValue(resultMap, mutop_vro));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
@@ -54,8 +59,9 @@ public class PRV_Test extends MethodTest {
 		String pre 	= "Object objtarget; RootClass rc = new RootClass(); SubClass1 sc1 = new SubClass1(); objtarget = rc; ";
 		String post = "Object objtarget; RootClass rc = new RootClass(); SubClass1 sc1 = new SubClass1(); objtarget = sc1; ";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_prv));
+		assertEquals(1, getApplicationValue(resultMap, mutop_vro));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
@@ -63,8 +69,9 @@ public class PRV_Test extends MethodTest {
 		String pre 	= "RootClass rctarget; RootClass rcsrc = new RootClass(); SubClass1 scsrc = new SubClass1(); rctarget = rcsrc; ";
 		String post = "RootClass rctarget; RootClass rcsrc = new RootClass(); SubClass1 scsrc = new SubClass1(); rctarget = scsrc; ";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_prv));
+		assertEquals(1, getApplicationValue(resultMap, mutop_vro));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
@@ -72,8 +79,9 @@ public class PRV_Test extends MethodTest {
 		String pre 	= "RootClass rcl = new RootClass(); SubClass1 sc1 = new SubClass1(); this.rc = rcl; System.out.println();";
 		String post = "RootClass rcl = new RootClass(); SubClass1 sc1 = new SubClass1(); this.rc = sc1; System.out.println();";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, getApplicationValue(resultMap, mutop)); 
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_prv)); 
+		assertEquals(1, getApplicationValue(resultMap, mutop_vro));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
@@ -81,16 +89,8 @@ public class PRV_Test extends MethodTest {
 		String pre 	= "Integer i = new Integer(4); this.obj = this.s1; ";
 		String post = "Integer i = new Integer(4); this.obj = i; ";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
-	}
-	
-	@Test
-	public void testPRV_NoMatching1() {
-		String pre 	= "Integer i = new Integer(4); Integer i2 = new Integer(5); this.obj = i; ";
-		String post = "Integer i = new Integer(4); Integer i2 = new Integer(5); this.obj = i2; ";
-		HashMap<String, Integer> resultMap = compareMatches(pre, post);
-		assertEquals(0, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_prv));
+		assertEquals(0, getApplicationValue(resultMap, mutop_vro));
+		checkOtherMutationOperators(resultMap);
 	}
 }
