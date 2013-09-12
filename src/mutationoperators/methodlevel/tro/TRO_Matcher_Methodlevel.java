d593007badb203735bfe3595ebb3f4e58d792f62
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-12 16:25:48 +0200

    Implemented code to detect a changed declared type of local variables

--- src/mutationoperators/methodlevel/tro/TRO_Matcher_Methodlevel.java
+++ src/mutationoperators/methodlevel/tro/TRO_Matcher_Methodlevel.java
@@ -3,10 +3,70 @@ package mutationoperators.methodlevel.tro;
 import mutationoperators.MutationOperator;
 import mutationoperators.TwoASTMatcher;
 
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
+import utils.ITypeBindingUtils;
+
 public class TRO_Matcher_Methodlevel extends TwoASTMatcher {
 
 	public TRO_Matcher_Methodlevel(MutationOperator mutop) {
 		super(mutop);
 	}
+
+	@Override
+	public boolean match(VariableDeclarationExpression node, Object other) {
+		// check if the other node is also a VariableDeclarationExpression
+		if(!(other instanceof VariableDeclarationExpression)) {
+			return false;
+		}
+		
+		// extract values
+		VariableDeclarationExpression node2 = (VariableDeclarationExpression) other;
+		ITypeBinding node_type_binding = node.getType().resolveBinding();
+		ITypeBinding node2_type_binding = node2.getType().resolveBinding();
+		
+		// check for conditions
+		boolean differentTypes = !(node.getType().subtreeMatch(defaultMatcher, node2.getType()));
+		boolean sameModifiers = node.modifiers().equals(node2.modifiers());
+		boolean compatibleTypes = ITypeBindingUtils.isTypeParentOfOtherType(node_type_binding, node2_type_binding);
+		
+		// if all conditions are true, notifiy a matching
+		if(differentTypes && sameModifiers && compatibleTypes) {
+			this.mutop.found(node, node2);
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean match(VariableDeclarationStatement node, Object other) {
+		// check if the other node is also a VariableDeclarationExpression
+		if(!(other instanceof VariableDeclarationStatement)) {
+			return false;
+		}
+		
+		// extract values
+		VariableDeclarationStatement node2 = (VariableDeclarationStatement) other;
+		ITypeBinding node_type_binding = node.getType().resolveBinding();
+		ITypeBinding node2_type_binding = node2.getType().resolveBinding();
+		
+		// check for conditions
+		boolean differentTypes = !(node.getType().subtreeMatch(defaultMatcher, node2.getType()));
+		boolean sameModifiers = node.modifiers().equals(node2.modifiers());
+		boolean compatibleTypes = ITypeBindingUtils.isTypeParentOfOtherType(node_type_binding, node2_type_binding);
+
+		// if all conditions are true, notifiy a matching
+		if(differentTypes && sameModifiers && compatibleTypes) {
+			this.mutop.found(node, node2);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
 
 }
--- src/mutationoperators/methodlevel/tro/TRO_Visitor_Methodlevel.java
+++ src/mutationoperators/methodlevel/tro/TRO_Visitor_Methodlevel.java
@@ -3,10 +3,46 @@ package mutationoperators.methodlevel.tro;
 import mutationoperators.TwoASTMatcher;
 import mutationoperators.TwoASTVisitor;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
 public class TRO_Visitor_Methodlevel extends TwoASTVisitor {
 
 	public TRO_Visitor_Methodlevel(TwoASTMatcher matcher) {
 		super(matcher);
 	}
 
+	@Override
+	public boolean visit(VariableDeclarationExpression node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+
+		if(localStoredTree instanceof VariableDeclarationExpression){
+			// cast other node
+			VariableDeclarationExpression vde = (VariableDeclarationExpression) localStoredTree;
+			
+			// check for an application
+			matcher.match(node, vde);
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean visit(VariableDeclarationStatement node) {
+		// locally store the AST
+		ASTNode localStoredTree = getParallelTree();
+
+		if(localStoredTree instanceof VariableDeclarationStatement){
+			// cast other node
+			VariableDeclarationStatement vds = (VariableDeclarationStatement) localStoredTree;
+
+			// check for an application
+			matcher.match(node, vds);
+		}
+		
+		return false;
+	}	
+	
 }
