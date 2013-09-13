3dc76b04a8899bb7cb20abbf4d4db2a4f2ac917b
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-13 17:01:56 +0200

    Reworked CRO_Test_SameCLass

--- testsrc/mutationoperators/CRO_Test_SameClass.java
+++ testsrc/mutationoperators/CRO_Test_SameClass.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 
+import mutationoperators.methodlevel.aco.ACO;
 import mutationoperators.methodlevel.cro.CRO;
 
 import org.junit.Test;
@@ -12,12 +13,15 @@ import utils.MethodTest;
 
 public class CRO_Test_SameClass extends MethodTest {
 
-	MutationOperator mutop;
+	MutationOperator mutop_cro;
+	MutationOperator mutop_aco;
 	
 	@Override
 	protected void initializeMutationOperatorsToTest() {
-		this.mutop = new CRO();
-		this.addMutationOperatorToTest(mutop);
+		this.mutop_cro = new CRO();
+		this.addMutationOperatorToTest(mutop_cro);
+		this.mutop_aco = new ACO();
+		this.addMutationOperatorToTest(mutop_aco);
 	}
 
 	@Override
@@ -37,29 +41,33 @@ public class CRO_Test_SameClass extends MethodTest {
 	@Test
 	public void testCRO_LocalReplacement1() {
 		HashMap<String, Integer> resultMap = compareMatches("Foo Temp = new Foo();", "Foo Temp = new Foo(1);");
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_cro));
+		assertEquals(1, getApplicationValue(resultMap, mutop_aco));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
 	public void testCRO_LocalReplacement2() {
 		HashMap<String, Integer> resultMap = compareMatches("Foo Temp = new Foo();", "Foo Temp = new Foo(1, null);");
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_cro));
+		assertEquals(1, getApplicationValue(resultMap, mutop_aco));
+		checkOtherMutationOperators(resultMap);
 	}
 
 	@Test
 	public void testCRO_SameArgumentNumber1() {
 		HashMap<String, Integer> resultMap = compareMatches("childRight = new Foo(); Foo Temp = new Foo(1);", "childRight = new Foo(); Foo Temp = new Foo(childRight);");
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_cro));
+		assertEquals(0, getApplicationValue(resultMap, mutop_aco));
+		checkOtherMutationOperators(resultMap);
 	}
 	
 	@Test
 	public void testCRO_SameArgumentNumber2() {
 		HashMap<String, Integer> resultMap = compareMatches("childRight = new Foo(); Foo Temp = new Foo(childRight, childRight);", "childRight = new Foo(); Foo Temp = new Foo(42, childRight);");
-		assertEquals(1, getApplicationValue(resultMap, mutop));
-		checkOtherMutationOperators(resultMap, mutop);
+		assertEquals(1, getApplicationValue(resultMap, mutop_cro));
+		assertEquals(0, getApplicationValue(resultMap, mutop_aco));
+		checkOtherMutationOperators(resultMap);
 	}
 
 }
