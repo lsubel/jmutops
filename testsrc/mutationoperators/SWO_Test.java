b008a76107692323814bfd042a615c3a181a16bf
Lukas Subel <s9lusube@stud.uni-saarland.de>
2013-09-14 15:05:43 +0200

    Correct alignment in SWO_Test to increase readability

--- testsrc/mutationoperators/SWO_Test.java
+++ testsrc/mutationoperators/SWO_Test.java
@@ -37,8 +37,8 @@ MutationOperator mutop;
 	
 	@Test
 	public void testSWO_if1() {
-		String pre = "int result; if(a >= 0) {result = a; System.out.println(\"Value is positive\");}else {result = - a; System.out.println(\"Value is negative\");} System.out.println(\"Finished method.\");";
-		String post = "int result; if(a >= 0) {result = - a; System.out.println(\"Value is negative\");}else {result = a; System.out.println(\"Value is positive\");} System.out.println(\"Finished method.\");";
+		String pre =  "int result; if(a >= 0) {result = a; System.out.println(\"Value is positive\");}else {result = - a; System.out.println(\"Value is negative\");} System.out.println(\"Finished method.\");";
+		String post = "int result; if(a >= 0) {result = -a; System.out.println(\"Value is negative\");}else {result = a; System.out.println(\"Value is positive\");} System.out.println(\"Finished method.\");";
 		HashMap<String, Integer> resultMap = compareMatches(pre, post);
 		assertEquals(1, getApplicationValue(resultMap, mutop));
 		checkOtherMutationOperators(resultMap, mutop);
