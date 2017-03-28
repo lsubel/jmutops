dfafb6acc448d926c5b6f83a05fa253b5adffd25
lsubel <lukas@lsubel.de>
2017-03-28 23:12:10 +0200

    Added README and LICENSE files

---
+++ README.mk
@@ -0,0 +1,16 @@
+# jmutops
+
+jMutOps is a Java tool that I developed during my computer science bachelor thesis at Saarland University in 2013. It has the ability to map detected source code changes between two different Java source code versions to 63 different mutation operators retrieved from research papers and existing mutation testing tools.
+
+For more details to this tool, please read the thesis "jMutOps: Mapping Changes to Mutation Operators".
+
+## Credits
+* [ChangeDistiller](https://bitbucket.org/sealuzh/tools-changedestiller/) by Software Evolution and Architecture Lab - University of Zurich
+* [Eclipse JDT](http://eclipse.org/jdt/overview.php) by Eclipse Foundation
+* [Apache Commons IO](https://commons.apache.org/io/) by Apache Foundation
+
+## Acknowledgement
+See acknowledgement section in my thesis.
+
+## License
+jMutOps is licensed under the [MIT license](https://github.com/lsubel/jmutops/blob/master/LICENSE.txt).
